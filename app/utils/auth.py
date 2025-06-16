6083fde622808fd63ff707cc95b5a52e9bdec95c
arvind-cs097 <166226576+arvind-cs097@users.noreply.github.com>
2025-06-16 20:20:57 +0530

    test_auth test update 10

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,16 +1,13 @@
-import os
+from flask import current_app
 import jwt
 from datetime import datetime, timedelta, timezone
-
-from dotenv import load_dotenv
-
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
 
 def generate_token(user_id):
     payload = {
         "user_id": user_id,
         "exp": datetime.now(timezone.utc) + timedelta(hours=1)
     }
+    
+    SECRET_KEY = current_app.config["SECRET_KEY"]
     print("Secret Key", SECRET_KEY)
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
