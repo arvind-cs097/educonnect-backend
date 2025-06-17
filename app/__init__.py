14b43fd9677743ec92c9b26c95e22bedc939c807
arvind-cs097 <166226576+arvind-cs097@users.noreply.github.com>
2025-06-17 21:53:21 +0530

    commenting tabel creation & role seeding update

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from app.extensions import db
 from flask_migrate import Migrate
 from config import config
 from .routes import all_blueprints
-from app.models import Role
+# from app.models import Role
 
 # db = SQLAlchemy()
 migrate = Migrate()
@@ -23,18 +23,18 @@ def create_app(config_class=None):
         app.register_blueprint(bp)
 
     # Create tables and seed roles (safe to run every time)
-    with app.app_context():
-        db.create_all()
-        seed_roles()
+    # with app.app_context():
+    #     db.create_all()
+    #     seed_roles()
 
     return app
 
-def seed_roles():
-    default_roles = ['admin', 'teacher', 'student']
+# def seed_roles():
+#     default_roles = ['admin', 'teacher', 'student']
 
-    for role_name in default_roles:
-        existing = Role.query.filter_by(name=role_name).first()
-        if not existing:
-            db.session.add(Role(name=role_name))
+#     for role_name in default_roles:
+#         existing = Role.query.filter_by(name=role_name).first()
+#         if not existing:
+#             db.session.add(Role(name=role_name))
 
-    db.session.commit()
+#     db.session.commit()
